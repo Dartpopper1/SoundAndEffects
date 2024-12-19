9c6ce03dbc14403ca4b52e5deec12c2eeca804ed
Dartpopper1 <espinozaj@roverkids.org>
2024-12-19 13:29:13 -0500

    Finished 3.3

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody playerRb;
     public float jumpForce = 10;
+    private Animator playerAnim;
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver = false;
@@ -14,15 +15,17 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver) {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         } }
 
         private void OnCollisionEnter(Collision collision)
@@ -34,6 +37,8 @@ public class PlayerController : MonoBehaviour
         {
             gameOver = true;
             Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
 
     }
